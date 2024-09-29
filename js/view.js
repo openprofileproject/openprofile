ec68e66385979dbd657dee98dcad51b60003825d
sctech <sctech@national.shitposting.agency>
2024-09-29 14:39:21 +0300

    Update view.js

--- js/view.js
+++ js/view.js
@@ -2,33 +2,58 @@ const params = new URLSearchParams(window.location.search);
 const userId = params.get('user_id');
 
 if (userId) {
-    const decodedData = JSON.parse(atob(userId)); // Base64 decode
+    const decodedData = JSON.parse(decodeBase62(userId)); // Base62 decode
     const profileInfo = document.getElementById('profile-info');
+
+    // Mapping for shortened keys
+    const keyMap = {
+        n: 'Name',
+        s: 'Surname',
+        g: 'Gender',
+        e: 'Email',
+        p: 'Phone',
+        w: 'Website',
+        gh: 'GitHub',
+        ig: 'Instagram',
+        tw: 'Twitter',
+        fb: 'Facebook',
+        yt: 'YouTube'
+    };
 
     for (const [key, value] of Object.entries(decodedData)) {
         const iconClass = {
-            name: 'fas fa-user',
-            surname: 'fas fa-user-tag',
-            gender: 'fas fa-venus-mars',
-            email: 'fas fa-envelope',
-            phone: 'fas fa-phone',
-            website: 'fas fa-globe',
-            github: 'fab fa-github',
-            instagram: 'fab fa-instagram',
-            twitter: 'fab fa-twitter',
-            facebook: 'fab fa-facebook',
-            youtube: 'fab fa-youtube'
+            n: 'fas fa-user',
+            s: 'fas fa-user-tag',
+            g: 'fas fa-venus-mars',
+            e: 'fas fa-envelope',
+            p: 'fas fa-phone',
+            w: 'fas fa-globe',
+            gh: 'fab fa-github',
+            ig: 'fab fa-instagram',
+            tw: 'fab fa-twitter',
+            fb: 'fab fa-facebook',
+            yt: 'fab fa-youtube'
         }[key];
 
         if (value) {
             profileInfo.innerHTML += `
                 <div class="profile-item">
                     <i class="${iconClass}"></i>
-                    <span>${key.charAt(0).toUpperCase() + key.slice(1)}: ${value}</span>
+                    <span>${keyMap[key]}: ${value}</span>
                 </div>
             `;
         }
     }
 } else {
     document.getElementById('profile-info').innerHTML = '<p>No user ID provided.</p>';
+}
+
+// Base62 decoding function
+function decodeBase62(str) {
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let num = BigInt(0);
+    for (let char of str) {
+        num = num * 62n + BigInt(chars.indexOf(char));
+    }
+    return Buffer.from(num.toString(16), 'hex').toString('utf8');
 }
