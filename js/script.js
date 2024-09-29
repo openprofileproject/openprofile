e30823dee89be1ddd5e1946ffb9ce76ad77cb7f5
sctech <sctech@national.shitposting.agency>
2024-09-29 14:38:12 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,23 @@ document.getElementById('profile-form').addEventListener('submit', function(e) {
 
     const formData = new FormData(this);
     const profileData = Object.fromEntries(formData.entries());
-    const encodedData = btoa(JSON.stringify(profileData)); // Base64 encode
+
+    // Use shortened keys
+    const compactData = {
+        n: profileData.name,
+        s: profileData.surname,
+        g: profileData.gender,
+        e: profileData.email,
+        p: profileData.phone,
+        w: profileData.website,
+        gh: profileData.github,
+        ig: profileData.instagram,
+        tw: profileData.twitter,
+        fb: profileData.facebook,
+        yt: profileData.youtube
+    };
+
+    const encodedData = encodeBase62(JSON.stringify(compactData)); // Base62 encode
 
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = `
@@ -13,3 +29,15 @@ document.getElementById('profile-form').addEventListener('submit', function(e) {
         <a href="view.html?user_id=${encodedData}" target="_blank">View Profile</a>
     `;
 });
+
+// Base62 encoding function
+function encodeBase62(str) {
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let num = BigInt('0x' + Buffer.from(str, 'utf8').toString('hex'));
+    let encoded = '';
+    while (num > 0) {
+        encoded = chars[num % 62n] + encoded;
+        num = num / 62n;
+    }
+    return encoded;
+}
